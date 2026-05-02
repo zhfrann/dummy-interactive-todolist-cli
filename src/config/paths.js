f4d0c3785d7ce91034f295b18f98ed6d8fff23c0
Muhammad Zhafran Ilham <muhzhafranilham13@gmail.com>
2026-05-02 15:49:30 +0700

    chore: failed black-box test setup

--- src/config/paths.js
+++ src/config/paths.js
@@ -1,7 +1,10 @@
 const path = require("path");
 
+const BASE_DIR = process.pkg
+    ? path.dirname(process.execPath)
+    : path.join(__dirname, "..", "..");
 /** Directory for JSON data storage. */
-const DATA_DIR = path.join(__dirname, "..", "..", "data");
+const DATA_DIR = path.join(BASE_DIR, "data");
 /** Absolute path to the todo JSON file. */
 const TODO_FILE = path.join(DATA_DIR, "todos.json");
 
